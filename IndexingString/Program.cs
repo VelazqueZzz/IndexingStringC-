2bb5bfdff33e44d1cb4c164fedf13ad860b85181
ITaly177 Milan <patrokl2010@yandex.ru>
2023-09-04 11:15:36 +0300

    Add project files.

---
+++ IndexingString/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+
+class InfexingStringDemo
+{
+    static void Main()
+    {
+        foreach(char s in "Текст")
+        {
+            Console.Write(s+" ");
+        }
+        Console.WriteLine();
+        for (int k = 0; k < "Текст".Length; k++)
+        {
+            Console.Write("Текст"[k] + "_");
+        }
+        Console.WriteLine();
+        string A = "Изучаем C#";
+        for(int k = 0; k < A.Length; k++)
+        {
+            Console.Write(A[k]);
+        }
+        Console.WriteLine();
+        showReversed(A);
+        Console.WriteLine(A);
+        String B = getReversed(A);
+        Console.WriteLine(B);
+
+    }
+    static String getReversed(String txt)
+    {
+        String str = "";
+        foreach (char s in txt)
+        {
+            str = s + str;
+        }
+        return str;
+    }
+        static void showReversed(String txt)
+        {
+            for(int k = txt.Length - 1; k >= 0; k--)
+            {
+                Console.Write(txt[k]);
+            }
+            Console.WriteLine();
+        }
+    
+}
